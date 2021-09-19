3054b28a1f4108630727a2f8342e5ad5ec37976e
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-19 23:14:42 +0430

    Add files via upload

--- silver.py
+++ silver.py
@@ -4,23 +4,23 @@ import time
 import arcade
 
 class Enemy(arcade.Sprite):
-    def __init__(self, w, h):
+    def __init__(self, w, h,speed):
         super().__init__(":resources:images/space_shooter/playerShip1_green.png")
-        self.speed = 4
+        self.speed = speed
         self.center_x = random.randint(0 , w)
         self.center_y = h
         self.angle = 180
-        self.width = 80
-        self.height = 80
+        self.width = 48
+        self.height = 48
         
     def move(self):
-        self.center_x += self.speed * math.cos(self.angle)
-        self.center_y += self.speed * math.sin(self.angle)
-
+       
+        self.center_y -= self.speed 
+        
 class Bullet(arcade.Sprite):
     def __init__(self, host):
         super().__init__(":resources:images/space_shooter/laserRed01.png") 
-        self.speed = 4
+        self.speed = 8
         self.angle = host.angle
         self.center_x = host.center_x
         self.center_y = host.center_y
@@ -40,7 +40,7 @@ class SpaceCraft(arcade.Sprite):
         self.center_y =48
         self.angle = 0
         self.change_angle = 0
-        self.bullet_list = [] 
+        self.bullet_list = []
         self.speed = 4
         self.jon = 3
         self.score = 0
@@ -59,27 +59,32 @@ class Game(arcade.Window):
         self.h = 600
         super().__init__(width=self.w,height=self.h,title="silver SpaceCraft YASAMIN")
         arcade.set_background_color(arcade.color.BLACK)
-        self.background_image=arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
+        self.background_image=arcade.load_texture("sayare.jpg")
         self.me = SpaceCraft(self.w, self.h)
-        self.enemy = Enemy (self.w , self.h)
         self.enemy_list =[]
         self.start_time = time.time()
         self.score = 0
-        #self.me.jon = 3
         self.num_enemy = 0
         self.me.jon_image = arcade.load_texture('R.jpg')
-       
+        self.speed = 1
     def on_draw(self):
         arcade.start_render()
+
+        if self.me.jon <= 0:
+            arcade.set_background_color(arcade.color.BLACK) 
+            arcade.draw_text("GAME OVER !", start_x = self.w //2,start_y = self.h //2,color = arcade.color.RED)
+            time.sleep(1)
+            return
         arcade.draw_lrwh_rectangle_textured(0, 0,self.w,self.h, self.background_image)
         self.me.draw()   
-        
+
         for i in range(len(self.me.bullet_list)):
             self.me.bullet_list[i].draw()
 
         for i in range(len(self.enemy_list)):
             self.enemy_list[i].draw()
         for i in range(self.me.jon):
+
             arcade.draw_lrwh_rectangle_textured(i*40 ,5 ,40 ,40 ,self.me.jon_image)
         arcade.draw_text(f"score: {self.score}", 700, 10, arcade.color.WHITE, 14)
     
@@ -89,24 +94,25 @@ class Game(arcade.Window):
         r = random.randrange(2,6,2)
         if self.end_time - self.start_time > r :
 
-          self.enemy_list.append(Enemy(self.w , self.h))
+          self.enemy_list.append(Enemy(self.w, self.h,self.speed))
           self.start_time = time.time()
-        
+        #self.enemy.move()
         self.num_enemy =+ 1
         self.me.rotate()
     
         for i in range(len(self.me.bullet_list)):
-            self.me.bullet_list[i].move()
+           self.me.bullet_list[i].move()
+
 
         for i in range(len(self.enemy_list)):
             self.enemy_list[i].move()
 
 
         for enemy in self.enemy_list:
-            for e in self.me.bullet_list:
-                if arcade.check_for_collision(enemy,e):
+            for bullet in self.me.bullet_list:
+                if arcade.check_for_collision(enemy,bullet):
                     self.enemy_list.remove(enemy)
-                    self.me.bullet_list.remove(e)
+                    self.me.bullet_list.remove(bullet)
                     self.score += 1
 
         for enemy in self.enemy_list:
@@ -115,11 +121,9 @@ class Game(arcade.Window):
                     print(self.me.jon.imag)
                     self.enemy_list.remove(enemy)
             
-        if self.me.jon <= 0:
-            arcade.draw_text('GAME OVER', self.w//2, self.h//2, arcade.color.RED, 50, width=100)
-            time.sleep(1)
-            exit()           
-   
+             
+
+        self.speed += 0.005
     def on_key_press(self, key, modifires):
         if key ==arcade.key.RIGHT:
             self.me.change_angle = -1
